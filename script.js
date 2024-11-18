cb3fdd78fbe1f806dac03f18b5671af4ef1ea6de
Jasmine <jasminetanjm@gmail.com>
2024-11-18 21:40:38 +0800

    update bg; refactor collision detection to stop detection while in endstate

--- script.js
+++ script.js
@@ -1,72 +1,82 @@
 const shigaraki = document.getElementById("shigaraki");
 const obstacle = document.getElementById("obstacle");
-const bg = document.getElementById("bg")
+const bg = document.getElementById("bg");
 const play = document.getElementById("playState");
 const text = document.getElementById("text");
 const scoreDisplay = document.getElementById("score");
 
 let score = 0;
-let gameOver = false;
-function playState(){
-    bg.classList.add("paused");
-    play.style.display = "block";
-    text.style.display = "none";
-    scoreDisplay.innerHTML = `Score: ${score}`;
-}
+let isAlive; // variable to store interval ID
 
-function endState(){
-    bg.classList.remove("startBg");
-    play.style.display = "none";
-    text.style.display = "block";
-    text.innerText = `Game Over!\nScore:${score}\nPress Enter to play again`;
-    score = 0;
-}
+// trigger jump() when user presses any key
+document.addEventListener("keydown", function (event) {
+  jump();
+});
 
-function jump(){
-    if (shigaraki.classList != "jump"){
-        shigaraki.classList.add("jump");
-        setTimeout(function() {
-            shigaraki.classList.remove("jump");
-        }, 500);
-    }
-}
+// trigger playState() when user clicks on the enter button
+document.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    playState();
+  }
+});
 
-let obstaclePassed = false;
-let isAlive = setInterval(function(){
+// playState(): game is running and character is moving
+function playState() {
+  // change display
+  bg.classList.add("startBg");
+  play.style.display = "block";
+  text.style.display = "none";
+  scoreDisplay.innerHTML = `Score: ${score}`;
+
+  // collision detection logic that will keep running every 10ms while in playState
+  let obstaclePassed = false;
+  isAlive = setInterval(function () {
     // get current shigaraki Y position
-    let shigarakiTop = parseInt(window.getComputedStyle(shigaraki).getPropertyValue("top"));
-    // console.log(shigarakiTop);
+    let shigarakiTop = parseInt(
+      window.getComputedStyle(shigaraki).getPropertyValue("top")
+    );
 
     // get current obstacle X position
-    let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
-    // console.log(obstacleLeft);
+    let obstacleLeft = parseInt(
+      window.getComputedStyle(obstacle).getPropertyValue("left")
+    );
 
     // detect collision
-    if (obstacleLeft < 60 && obstacleLeft > 40 && shigarakiTop >= 140){
-        //collision hppens
-        console.log("collision");
-        endState();
-        // alert("Game Over!\nPress Enter to play again");
-    } else if (obstacleLeft < 40 && !obstaclePassed){
-        score++;
-        scoreDisplay.innerHTML = `Score: ${score}`;
-        obstaclePassed = true;
-    } else if (obstacleLeft > 60){
-        obstaclePassed = false;
+    if (obstacleLeft < 60 && obstacleLeft > 40 && shigarakiTop >= 140) {
+      // stop collision detection before going into endState
+      clearInterval(isAlive);
+      endState();
+    } else if (obstacleLeft < 40 && !obstaclePassed) {
+      // shigaraki passed the obstacle
+      score++;
+      scoreDisplay.innerHTML = `Score: ${score}`;
+      obstaclePassed = true;
+    } else if (obstacleLeft > 60) {
+      // obstacle is off screen
+      obstaclePassed = false;
     }
-}, 10);
-
-
-function obstacleRandomizer(){
-    // TODO: randomize obstacle image
+  }, 10);
 }
 
-document.addEventListener("keydown", function(event){
-    jump();
-});
+// endState(): game is over and character is not moving
+function endState() {
+  bg.classList.remove("startBg");
+  play.style.display = "none";
+  text.style.display = "block";
+  text.innerText = `Game Over!\nScore:${score}\nPress Enter to play again`;
+  score = 0;
+}
 
-document.addEventListener("keypress", function(event){
-    if (event.key === 'Enter') {
-        playState();
-    }
-});
+// jump(): make the character jump
+function jump() {
+  if (shigaraki.classList != "jump") {
+    shigaraki.classList.add("jump");
+    setTimeout(function () {
+      shigaraki.classList.remove("jump");
+    }, 500);
+  }
+}
+
+function obstacleRandomizer() {
+  // TODO: randomize obstacle image
+}
